ceaed72c8934bbc6babe641c7639f7f3d978b1a2
Szymon Acedański <accek@mimuw.edu.pl>
2012-09-02 15:06:50 +0200

    consistent versions in setup.py and docs, per PEP 386

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "filetracker",
-    version = 'dev',
+    version = '1.0.dev',
     author = "SIO2 Project Team",
     author_email = 'sio2@sio2project.mimuw.edu.pl',
     description = "Filetracker caching file storage",
