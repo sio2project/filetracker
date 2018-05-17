516686e9c8234a327ab2a17a24af88c632b65b3c
Pavel Senchanka <pavel.senchanka@gmail.com>
2018-05-17 23:13:46 +0200

    Removed filetracker 1.0 compatible API reexports.

--- filetracker/dummy.py
+++
@@ -1,5 +0,0 @@
-# 'client.dummy' package is reexported here to prevent 
-# breaking oioioi. oioioi.filetracker package should be adapted to
-# use new paths.
-
-from filetracker.client.dummy import *
