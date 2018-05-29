98739140277a8894b010d870f921c1621ef9baa5
Pavel Senchanka <pavel.senchanka@gmail.com>
2018-05-29 19:05:48 +0200

    Bumped version to 2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'filetracker',
-    version = '2.1rc1',
+    version = '2.1',
     author = 'SIO2 Project Team',
     author_email = 'sio2@sio2project.mimuw.edu.pl',
     description = 'Filetracker caching file storage',
